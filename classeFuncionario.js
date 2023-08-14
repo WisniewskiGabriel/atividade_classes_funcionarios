664fc3880c71ebc38079ff2cb5119e6c66659a56
Gabriel <gabwisniewski@gmail.com>
2023-08-14 03:06:20 -0300

    propriedade 'trabalhando' e metodo 'trabalhar'

--- classeFuncionario.js
+++ classeFuncionario.js
@@ -1,8 +1,9 @@
 class Funcionario{
-    constructor(nome,idade,cargo){
+    constructor(nome,idade,cargo,trabalhando){
         this._nome = nome;
         this._idade = idade;
         this._cargo = cargo;
+        this._trabalhando = trabalhando;
     }
 
     get nome(){
@@ -35,6 +36,14 @@ class Funcionario{
         "tenho "+this._idade+" ano"+(Number(this.idade)>1 ? 's' : '')+" "+
         "e trabalho nessa empresa no cargo de "+this._cargo;
         console.log(strApresentacao);
+    }
+
+    trabalhar(bool){
+        if(this.trabalhando = true && bool){
+            console.log("Já está trabalhando.");
+        } else {
+            this.trabalhando = bool;
+        }
     }
 }
 module.exports = Funcionario;
