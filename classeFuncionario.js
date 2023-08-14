123c834a6f43ce4c4e4dc74c32cfa1752655577e
Gabriel <gabwisniewski@gmail.com>
2023-08-14 02:10:45 -0300

    getters e setters da classe Funcionario

--- classeFuncionario.js
+++ classeFuncionario.js
@@ -4,4 +4,28 @@ class Funcionario{
         this.idade = idade;
         this.cargo = cargo;
     }
+
+    get nome(){
+        return this.nome;
+    }
+
+    set nome(novoNome){
+        this.nome = novoNome
+    }
+
+    get idade(){
+        return this.idade;
+    }
+
+    set idade(novaIdade){
+        this.idade = novoIdade
+    }
+
+    get cargo(){
+        return this.cargo;
+    }
+
+    set cargo(novoCargo){
+        this.cargo = novoCargo
+    }
 }
