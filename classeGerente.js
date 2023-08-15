f851a528ae83c2ac149cb3a35d086f829a849c7c
Gabriel <gabwisniewski@gmail.com>
2023-08-15 02:38:06 -0300

    fix console.log comentado sobre departament de gte

--- classeGerente.js
+++ classeGerente.js
@@ -20,7 +20,7 @@ class Gerente extends Funcionario{
             console.log("Já está gerenciando");
         }else{
             this._gerenciando = true;
-            //console.log("Gerenciamento iniciado no departamento "+this._departamento);
+            console.log("Gerenciamento iniciado no departamento "+this._departamento);
         }
     }
 }
